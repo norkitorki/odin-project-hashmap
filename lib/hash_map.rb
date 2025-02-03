55a996785660f139591ba10c7359f5fafe7dca24
norkitorki <norki@tuta.io>
2025-02-03 11:44:13 +0100

    Finalize hash map functionality

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,9 +1,108 @@
 # frozen_string_literal: true
 
+require_relative 'linked_list'
+
 class HashMap
+  attr_reader :buckets
+
   def initialize
-    @load_factor = 0.8
     @capacity    = 16
     @buckets     = Array.new(@capacity)
+    @node_count  = 0
+    assign_load_factor
+  end
+
+  def hash(key)
+    key.to_s.chars.reduce(0) { |h, c| (31 * h) + c.ord }
+  end
+
+  def set(key, value)
+    insert_item(key, value)
+    resize_buckets('append')
+  end
+
+  def get(key)
+    find_list(key)&.find(key)&.value
+  end
+
+  def has?(key)
+    !get(key).nil?
+  end
+
+  def remove(key)
+    value = find_list(key)&.remove(key)&.value
+    return unless value
+
+    @node_count -= 1
+    resize_buckets('remove')
+    value
+  end
+
+  def length
+    @node_count
+  end
+
+  def clear
+    initialize && true
+  end
+
+  def keys
+    collect_properties([:key])
+  end
+
+  def values
+    collect_properties([:value])
+  end
+
+  def entries
+    collect_properties(%i[key value])
+  end
+
+  private
+
+  def assign_load_factor
+    @load_factor = @capacity * 0.75
+  end
+
+  def check_buckets_in_bounds(index)
+    raise IndexError if index.negative? || index >= @buckets.length
+  end
+
+  def find_index(key)
+    index = hash(key) % @capacity
+    check_buckets_in_bounds(index)
+    index
+  end
+
+  def find_list(key)
+    index = find_index(key)
+    @buckets[index]
+  end
+
+  def collect_properties(props = [])
+    arr = []
+    @buckets.each { |l| l.to_a.each { |n| arr << (props.length > 1 ? props.map { |p| n[p] } : n[props.first]) } }
+    arr
+  end
+
+  def insert_item(key, value)
+    index = find_index(key)
+    @buckets[index] ? @buckets[index].add(key, value) : @buckets[index] = LinkedList.new(key, value)
+    @node_count += 1
+  end
+
+  def change_capacity(operation)
+    operation == 'append' ? @capacity *= 2 : (@capacity /= 2 unless @capacity / 2 < 16)
+  end
+
+  def resize_buckets(operation)
+    return unless operation == 'append' ? @node_count > @load_factor : @node_count > 16 && @node_count < @load_factor
+
+    change_capacity(operation)
+    old_buckets = @buckets
+    @buckets = Array.new(@capacity)
+    @node_count = 0
+    assign_load_factor
+    old_buckets.each { |list| list.to_a.each { |node| insert_item(node[:key], node[:value]) } }
   end
 end
